a8a62a4da69928b964021a011f70e134debb2081
Javier Galarza <jegj57@gmail.com>
2024-04-12 05:22:34 -0500

    chore: add some exercises

---
+++ learning_go/chapter2/e1/e1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var i int = 20
+	var f float64 = float64(i)
+
+	fmt.Printf("i %v and f %v\n", i, f)
+}
---
+++ learning_go/chapter2/e2/e2.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const value = 10
+
+	var i int = value
+	var f float64 = value
+
+	fmt.Printf("i %v and f %v\n", i, f)
+}
