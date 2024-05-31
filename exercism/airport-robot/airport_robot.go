20ab2f1945221b36928535c5105e924590316f50
Javier Galarza <jegj57@gmail.com>
2024-05-31 05:28:48 -0500

    chore: new one

---
+++ exercism/airport-robot/airport_robot.go
@@ -0,0 +1,36 @@
+package airportrobot
+
+import "fmt"
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+func SayHello(name string, greeeter Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", greeeter.LanguageName(), greeeter.Greet(name))
+}
+
+type Italian struct{}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+type Portuguese struct{}
+
+func (i Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (i Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}
+
+// Write your code here.
+// This exercise does not have tests for each individual task.
+// Try to solve all the tasks first before running the tests.
