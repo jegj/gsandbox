b01a9877167ebfb623f944d69fdd294a336a25ae
Javier Galarza <jegj57@gmail.com>
2024-06-04 05:52:12 -0500

    chore: add new section

---
+++ servers/e1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// curl http://localhost:8090/hello?name=world
+func hello(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if name := req.FormValue("name"); name != "" {
+		fmt.Fprintf(w, "hello %s\n", name)
+	}
+}
+
+func main() {
+	fmt.Println("server running...")
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
