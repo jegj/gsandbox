c49f7b7dd87cdbe98d6e514fed25347dc1d6a601
Javier Galarza <jegj57@gmail.com>
2024-06-07 05:48:32 -0500

    chore: resolve one more

---
+++ exercism/interest-is-interesting/interest_is_interesting.go
@@ -0,0 +1,37 @@
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	if balance < 0 {
+		return 3.213
+	} else if balance >= 0 && balance < 1000 {
+		return 0.5
+	} else if balance >= 1000 && balance < 5000 {
+		return 1.621
+	} else {
+		return 2.475
+	}
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	rate := float64(InterestRate(balance) / 100)
+	return balance * rate
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	interest := Interest(balance)
+	return balance + interest
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	years := 0
+	nbalance := balance
+	for nbalance < targetBalance {
+		nbalance = AnnualBalanceUpdate(nbalance)
+		years++
+	}
+	return years
+}
