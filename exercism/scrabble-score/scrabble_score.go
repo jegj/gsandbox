c440853bec9ae5ef7b0c28bee7e39214de2f717b
Javier Galarza <jegj57@gmail.com>
2024-06-26 05:54:13 -0500

    chore: add new one

---
+++ exercism/scrabble-score/scrabble_score.go
@@ -0,0 +1,28 @@
+package scrabble
+
+import "unicode"
+
+func Score(word string) int {
+	score := 0
+	for _, letter := range word {
+		switch unicode.ToLower(letter) {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			score += 1
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+		default:
+			score += 0
+		}
+	}
+	return score
+}
