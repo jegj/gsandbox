4a93fc7eff00b0d5afa7be7a033732426093ebb6
Javier Galarza <jegj57@gmail.com>
2024-07-08 05:28:55 -0500

    chore: make it better

--- exercism/nucleotide-count/nucleotide_count.go
+++ exercism/nucleotide-count/nucleotide_count.go
@@ -4,7 +4,7 @@ import "errors"
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
 // Choose a suitable data type.
-type Histogram map[rune]int
+type Histogram map[rune]uint
 
 // DNA is a list of nucleotides. Choose a suitable data type.
 type DNA string
@@ -16,7 +16,7 @@ type DNA string
 // The receiver appears in its own argument list between the func keyword and the method name.
 // Here, the Counts method has a receiver of type DNA named d.
 func (d DNA) Counts() (Histogram, error) {
-	var h Histogram = map[rune]int{
+	var h Histogram = Histogram{
 		'A': 0,
 		'C': 0,
 		'G': 0,
