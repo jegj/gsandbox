209ea26c17cfbcaafbb21646b9e2d125f6fd3ac7
Javier Galarza <jegj57@gmail.com>
2024-07-18 05:17:58 -0500

    chore: improve solution

--- exercism/etl/etl.go
+++ exercism/etl/etl.go
@@ -1,12 +1,14 @@
 package etl
 
-import "unicode"
+import (
+	"strings"
+)
 
 func Transform(in map[int][]string) map[string]int {
 	result := make(map[string]int)
 	for value, letters := range in {
 		for _, letter := range letters {
-			result[string(unicode.ToLower(rune(letter[0])))] = value
+			result[strings.ToLower(letter)] = value
 		}
 	}
 	return result
